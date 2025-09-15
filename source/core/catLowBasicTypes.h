7a27a2482654eedc64f0217249654e37b6a68f1a
猫大名ねこ猫 <48146054+W88DodPECuThLOl@users.noreply.github.com>
2025-09-15 11:40:55 +0900

    feat: :sparkles: picoでのビルドに対応
    - Raspberry Pi Picoの基本的な型の定義を追加
    - core部分のビルドができるようになった

--- source/core/catLowBasicTypes.h
+++ source/core/catLowBasicTypes.h
@@ -36,6 +36,39 @@ typedef unsigned long long size_t; // --target = wasm64
 static_assert(false) // 未対応
 #endif
 
+#elif defined(PICO_BUILD) && !!PICO_BUILD
+
+// Raspberry Pi Pico/Pico2
+
+#include <cstdint>
+
+// 基本的な型
+typedef std::int8_t   s8;
+typedef std::uint8_t  u8;
+typedef std::int16_t  s16;
+typedef std::uint16_t u16;
+typedef std::int32_t  s32;
+typedef std::uint32_t u32;
+typedef std::int64_t  s64;
+typedef std::uint64_t u64;
+typedef float         f32;
+typedef double        f64;
+#if __cpp_char8_t
+typedef char8_t       c8;
+#else
+typedef char          c8;
+#endif // __cpp_char8_t
+typedef char16_t      c16;
+typedef char32_t      c32;
+
+#if defined(PICO_32BIT) && !!PICO_32BIT
+// 32bit
+typedef std::uint32_t size_t;
+#else
+// 64bit
+typedef std::uint64_t size_t;
+#endif
+
 #else
 
 // VC++
