6511c6ec06f4f1d2f18e5dd2ea9bde219596f60f
Ken Mitton <mittonk@alum.mit.edu>
2025-12-17 17:37:44 -0500

    libretro: analog controls, multiplayer, and course switch

--- source/libretro/LibretroPD777.cpp
+++ source/libretro/LibretroPD777.cpp
@@ -1,7 +1,19 @@
+#include <cstdio>
 #include "LibretroPD777.h"
 #include "libretro.h"
 
 extern retro_video_refresh_t video_cb;
+
+namespace {
+
+    inline f32 clamp(f32 value, const f32 minValue, const f32 maxValue)
+    {
+        if(value < minValue) { return minValue; }
+        if(value > maxValue) { return maxValue; }
+        return value;
+    }
+
+} // namespace
 
 LibretroPD777::LibretroPD777()
     : PD777()
@@ -50,6 +62,33 @@ void LibretroPD777::reset()
     regs.reset();
     sound.reset();
     calledVBLK = false;
+} 
+
+bool LibretroPD777::isPD1(u8& value) {
+    padValue[0] = clamp(padValue[0] + float(analogStatus.input_analog_left_y[0]) / analogscale, KeyStatus::PAD_MIN_VALUE, KeyStatus::PAD_MAX_VALUE);
+    value = (u8)((u8)padValue[0] & 0x7F);
+    return true;
+}
+
+bool LibretroPD777::isPD2(u8& value)
+{
+    padValue[1] = clamp(padValue[1] + float(analogStatus.input_analog_left_x[0]) / analogscale, KeyStatus::PAD_MIN_VALUE, KeyStatus::PAD_MAX_VALUE);
+    value = (u8)((u8)padValue[1] & 0x7F);
+    return true;
+}
+
+bool LibretroPD777::isPD3(u8& value)
+{
+    padValue[2] = clamp(padValue[2] + float(analogStatus.input_analog_left_x[1]) / analogscale, KeyStatus::PAD_MIN_VALUE, KeyStatus::PAD_MAX_VALUE);
+    value = (u8)((u8)padValue[2] & 0x7F);
+    return true;
+}
+
+bool LibretroPD777::isPD4(u8& value)
+{
+    padValue[3] = clamp(padValue[3] + float(analogStatus.input_analog_left_y[1]) / analogscale, KeyStatus::PAD_MIN_VALUE, KeyStatus::PAD_MAX_VALUE);
+    value = (u8)((u8)padValue[3] & 0x7F);
+    return true;
 }
 
 void LibretroPD777::setFLS(const s64 clockCounter, const u8 value, const bool reverberatedSoundEffect)
